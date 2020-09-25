76ed4a100e04ca55807d2e1d2396e3bbee0fbecf
Gregory Vincic <g@7de.se>
2020-09-25 07:50:50 +0200

    Update package docs

--- assert.go
+++ assert.go
@@ -1,16 +1,3 @@
-/* package defines AssertFunc wrapper of testing.T
-
-Online assertions are done by wrapping the T in a test
-
-    func TestSomething(t *testing.T) {
-        assert := asserter.New(t)
-        got, err := Something()
-        t.Logf("%v, %v := Something()", got, err)
-        assert(err == nil).Fail()
-        // Special case used very often is check equality
-        assert().Equals(got, 1)
-    }
-*/
 package asserter
 
 import (
---
+++ package.go
@@ -0,0 +1,17 @@
+/*
+
+Package asserter defines helpers for asserting various outputs.
+
+Online assertions are done by wrapping the T in a test
+
+    func TestSomething(t *testing.T) {
+        assert := asserter.New(t)
+        got, err := Something()
+        t.Logf("%v, %v := Something()", got, err)
+        assert(err == nil).Fail()
+        // Special case used very often is check equality
+        assert().Equals(got, 1)
+    }
+
+*/
+package asserter
